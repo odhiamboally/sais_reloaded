0ccccbed349724c5dd53d6efddf485839b52b4f0
Allan Alex <36701027+odhiamboally@users.noreply.github.com>
2025-06-03 00:41:49 +0300

    SQL Data Script

---
+++ insert_programmes.sql
@@ -0,0 +1,7 @@
+INSERT INTO dbo.Programmes (Name, IsDeleted, DeletedAt)
+VALUES
+  ('Orphans and vulnerable children', 0, NULL),
+  ('Poor elderly persons',       0, NULL),
+  ('Persons with disability',    0, NULL),
+  ('Persons in extreme poverty', 0, NULL),
+  ('Any other',                  0, NULL);
